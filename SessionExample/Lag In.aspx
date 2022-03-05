316ad1f20fec89d1621aa218eb2adcee95c7de41
DURGA <DURGA@LAPTOP-59233956>
2022-03-05 22:39:06 +0530

    Add project files.

---
+++ SessionExample/Lag In.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Lag In.aspx.cs" Inherits="SessionExample.Lag_In" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <table align ="center">
+                
+                <tr>
+                    <td>User Name</td>
+                    <td>
+                    <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>Passoword</td>
+                    <td>
+                        <asp:TextBox ID="txtPwd" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="btnLogin" runat="server" Text="Log In" OnClick="btnLogin_Click" /></td>
+                    <td><asp:Button ID="btnReset" runat="server" Text="Reset" OnClick="btnReset_Click" /></td>
+                    
+                </tr>
+            </table>
+
+
+        </div>
+        <center><asp:Label ID="lblDisplay" runat="server" ForeColor="Red"></asp:Label>
+         <asp:Label ID="newlbl" runat="server" ForeColor="Orange"></asp:Label>
+    </center>
+
+    </form>
+</body>
+</html>
---
+++ SessionExample/Lag In.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionExample
+{
+    
+    public partial class Lag_In : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["strConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            SutdentInfo _objSutdentInfo = new SutdentInfo(txtUserName.Text, txtPwd.Text);
+            Session["objSutdentInfo"] = _objSutdentInfo;
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+            SqlCommand cmd;
+            SqlDataReader sdr;
+            cmd = new SqlCommand("Select UserName, Pwd From UserDetails", con);
+            sdr = cmd.ExecuteReader();
+
+            while(sdr.Read())
+            {
+                if(sdr["UserName"].ToString()==txtUserName.Text && sdr["Pwd"].ToString() == txtPwd.Text)
+                {
+                    string sKey = txtUserName.Text + txtPwd.Text;
+                    string sUser = Convert.ToString(Cache[sKey]);
+
+                    if (sUser == null || sUser == string.Empty)
+                    {
+                        TimeSpan SessTimeOut = new TimeSpan(0, 0, HttpContext.Current.Session.Timeout, 0, 0);
+                        HttpContext.Current.Cache.Insert(sKey, sKey, null, DateTime.MaxValue, SessTimeOut, System.Web.Caching.CacheItemPriority.NotRemovable, null);
+                        Session["User"] = txtUserName.Text + txtPwd.Text;
+                        Response.Redirect("Home.aspx");
+                    }
+                    else
+                    {
+                        lblDisplay.Text = "<Marquee><h1><font color=red>Already Logged IN</font></h1></marquee>";
+                        return;
+                    }
+                }
+                else
+                {
+                    lblDisplay.Text = "Invalid Credentials try again !!";
+                }
+
+            }
+
+        }
+
+        [Serializable]
+        public class SutdentInfo
+        {
+            public SutdentInfo()
+            { 
+            
+            }
+
+            public SutdentInfo(string UserName, string Password)
+            {
+                this.Uname = UserName;
+                this.Pwd = Password;
+            }
+
+            private string UserName;
+            private string Password;
+            public string Uname
+            {
+                get { return UserName; }
+                set { UserName = value; }
+            }
+            public string Pwd
+            {
+                get { return Password; }
+                set { Password = value; }
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtUserName.Text = "";
+            txtPwd.Text = "";
+        }
+    }
+}
